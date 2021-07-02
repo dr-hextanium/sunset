55c639e5cd33f7fb4315c799b08646cb6870cd6e
Hextanium <hextanium.on.discord@gmail.com>
2021-07-02 09:46:14 -0400

    [add] add utilities

---
+++ utilities/utils.js
@@ -0,0 +1,11 @@
+module.exports.backspace = (count, robot) => {
+    for (let i = 0; i < count; i++) {
+        robot.keyTap("backspace");
+    }
+}
+
+module.exports.control = (key, robot) => {
+    robot.keyToggle("control", "down");
+    robot.keyTap(key);
+    robot.keyToggle("control, up");
+}
